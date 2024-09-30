f67001c5374079c350394c80503e0af95a2eab4a
Jodking <tripathihitesh580@gmail.com>
2024-09-30 12:50:03 +0530

    env

--- src/fetch/FetchData.js
+++ src/fetch/FetchData.js
@@ -1,14 +1,11 @@
-const CLOUDFLARE_WORKER_URL =
-  "https://swiggy-proxy.tripathihitesh580.workers.dev";
+const CLOUDFLARE_WORKER_URL = import.meta.env.VITE_CLOUDFLARE_WORKER_URL;
 
 export const FetchData = async () => {
   try {
-    // Get location from localStorage or use default place_id
     const loca = JSON.parse(localStorage.getItem("selectedLocation")) || {
       place_id: "ChIJwe1EZjDG5zsRaYxkjY_tpF0",
     };
 
-    // Fetch the longitude and latitude using the place_id
     const logresponse = await fetch(
       `${CLOUDFLARE_WORKER_URL}/misc?place_id=${loca.place_id}`
     );
@@ -17,7 +14,6 @@ export const FetchData = async () => {
     const { lng = 72.8856, lat = 19.0748 } =
       logresult.data[0]?.geometry.location || {};
 
-    // Fetch restaurant data based on longitude and latitude
     const response = await fetch(
       `${CLOUDFLARE_WORKER_URL}/restaurants?lat=${lat}&lng=${lng}`
     );
@@ -26,18 +22,16 @@ export const FetchData = async () => {
     return result;
   } catch (error) {
     console.error("Error fetching restaurant data:", error);
-    return null; // Handle error gracefully
+    return null;
   }
 };
 
 export const FetchMenu = async (id) => {
   try {
-    // Reuse location logic from FetchData
     const loca = JSON.parse(localStorage.getItem("selectedLocation")) || {
       place_id: "ChIJwe1EZjDG5zsRaYxkjY_tpF0",
     };
 
-    // Fetch longitude and latitude using the place_id
     const logresponse = await fetch(
       `${CLOUDFLARE_WORKER_URL}/misc?place_id=${loca.place_id}`
     );
@@ -46,7 +40,6 @@ export const FetchMenu = async (id) => {
     const { lng = 72.8856, lat = 19.0748 } =
       logresult.data[0]?.geometry.location || {};
 
-    // Fetch menu data based on longitude, latitude, and restaurant id
     const data = await fetch(
       `${CLOUDFLARE_WORKER_URL}/menu?lat=${lat}&lng=${lng}&id=${id}`
     );
@@ -55,6 +48,6 @@ export const FetchMenu = async (id) => {
     return result;
   } catch (error) {
     console.error("Error fetching menu data:", error);
-    return null; // Handle error gracefully
+    return null;
   }
 };
